20bcd297b61d90210f7bf5abc0ecff8baac0cab0
Phyllip Hall <phall776@gmail.com>
2023-09-06 22:09:31 -0400

    feat: NavTree final

--- src/lib/components/nav/NavTree.svelte
+++ src/lib/components/nav/NavTree.svelte
@@ -13,15 +13,25 @@
 			{#if node.indent === 0}
 				<NavTreeItem on:nodeProcessed {node} indent={'ml-0'} />
 			{:else if node.indent === 1}
-				<NavTreeItem on:nodeProcessed {node} indent={'ml-4'} />
+				<NavTreeItem on:nodeProcessed {node} indent={'ml-3'} />
 			{:else if node.indent === 2}
-				<NavTreeItem on:nodeProcessed {node} indent={'ml-8'} />
+				<NavTreeItem on:nodeProcessed {node} indent={'ml-6'} />
 			{:else if node.indent === 3}
-				<NavTreeItem on:nodeProcessed {node} indent={'ml-12'} />
+				<NavTreeItem on:nodeProcessed {node} indent={'ml-9'} />
 			{:else if node.indent === 4}
-				<NavTreeItem on:nodeProcessed {node} indent={'ml-16'} />
+				<NavTreeItem on:nodeProcessed {node} indent={'ml-12'} />
 			{:else if node.indent === 5}
-				<NavTreeItem on:nodeProcessed {node} indent={'ml-20'} />
+				<NavTreeItem on:nodeProcessed {node} indent={'ml-15'} />
+			{:else if node.indent === 6}
+				<NavTreeItem on:nodeProcessed {node} indent={'ml-18'} />
+			{:else if node.indent === 7}
+				<NavTreeItem on:nodeProcessed {node} indent={'ml-21'} />
+			{:else if node.indent === 8}
+				<NavTreeItem on:nodeProcessed {node} indent={'ml-24'} />
+			{:else if node.indent === 9}
+				<NavTreeItem on:nodeProcessed {node} indent={'ml-27'} />
+			{:else if node.indent === 10}
+				<NavTreeItem on:nodeProcessed {node} indent={'ml-30'} />
 			{/if}
 		{/each}
 	</div>
