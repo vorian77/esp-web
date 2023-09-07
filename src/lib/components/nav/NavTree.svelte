fe20a6875f0526d3f935b303d5cfcb836f0e37e0
Phyllip Hall <phall776@gmail.com>
2023-09-06 21:31:08 -0400

    bug: NavTree indent

--- src/lib/components/nav/NavTree.svelte
+++ src/lib/components/nav/NavTree.svelte
@@ -13,23 +13,45 @@
 	}
 </script>
 
-<div class="mx-2 mb-2 text-sm">
+<div class="mx-2 mb-2">
 	<div class="bg-slate-300 rounded-lg p-1 mb-1">Features:</div>
 
 	<div>
 		{#each $navNodesTree as node, i}
 			{@const rowIndent = [0, 4, 8, 12, 16][node.indent]}
-			<div
-				class="ml-{rowIndent} p-1 mb-1 hover:bg-blue-400 rounded-lg
+			{#if node.indent === 0}
+				<div
+					class="ml-0 p-1 mb-1 hover:bg-blue-400 rounded-lg
 				{node.selected ? 'bg-blue-300 text-white' : ''}"
-				role="button"
-				tabindex="0"
-				on:click={() => processNode(node)}
-				on:keyup={() => processNode(node)}
-			>
-				{node.label}
-				{rowIndent}
-			</div>
+					role="button"
+					tabindex="0"
+					on:click={() => processNode(node)}
+					on:keyup={() => processNode(node)}
+				>
+					{node.label}
+				</div>
+			{:else if node.indent === 1}
+				<div
+					class="ml-4 p-1 mb-1 hover:bg-blue-400 rounded-lg
+				{node.selected ? 'bg-blue-300 text-white' : ''}"
+					role="button"
+					tabindex="0"
+					on:click={() => processNode(node)}
+					on:keyup={() => processNode(node)}
+				>
+					{node.label}
+				</div>
+			{:else if node.indent === 2}
+				<div
+					class="ml-8 p-1 mb-1 hover:bg-blue-400 rounded-lg
+				{node.selected ? 'bg-blue-300 text-white' : ''}"
+					role="button"
+					tabindex="0"
+					on:click={() => processNode(node)}
+					on:keyup={() => processNode(node)}
+				>
+					{node.label}
+				</div>{/if}
 		{/each}
 	</div>
 </div>
