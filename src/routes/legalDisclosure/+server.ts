13435b8c5ab220408acb5a46ed01334c566d7b5b
Phyllip Hall <phall776@gmail.com>
2023-09-19 17:59:06 -0400

    maint: move DB access from client to server

--- src/routes/legalDisclosure/+server.ts
+++ src/routes/legalDisclosure/+server.ts
@@ -1,6 +1,20 @@
+import { HTMLMETHOD } from '$comps/types'
 import { dbESPAPI } from '$server/dbESP'
+import { asUpsert, asGet } from '$lib/utils/utils'
 
 export async function POST({ request }) {
-	const { method, procedure, data } = await request.json()
+	// reset user object in locals to force re-retrieval
+	asUpsert('user', undefined)
+
+	// update user's disclosure flag
+	const method = HTMLMETHOD.POST
+	const procedure = 'ws_cm_ssr_disclosure'
+
+	const { applicantId } = await request.json()
+	const data = {
+		applicantId,
+		disclosure: 1
+	}
+
 	return await dbESPAPI(method, procedure, data)
 }
