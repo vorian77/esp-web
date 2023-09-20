c9faae66c0cda9839f7fce429d6b93eb248226f9
Phyllip Hall <phall776@gmail.com>
2023-09-20 08:58:13 -0400

    maint: match edgeDB properties to existing Form

--- src/lib/server/dbEdgeFunctions.ts
+++ src/lib/server/dbEdgeFunctions.ts
@@ -1,16 +1,45 @@
-import {
-	EdgeQL,
-	EdgeDBDataType,
-	EdgeDBFilterDirection,
-	dbSelect,
-	dbSelectSingle
-} from '$server/dbEdgeCore'
+import { EdgeQL, dbExecute, dbSelect, dbSelectSingle } from '$server/dbEdgeCore'
+import { EdgeDBDataType, EdgeDBFilterDirection, type FormAction } from '$comps/types'
 
 const FILENAME = 'server/dbEdgeFunctions.ts'
 
+export async function getForm(formId: string, formActionType: 'select' | 'insert', data: {}) {
+	const formDefn = await getFormDefn(formId)
+	const formData = await getFormData(formActionType, formDefn.actions, data)
+	return { formDefn, formData }
+
+	async function getFormDefn(formId: string) {
+		const query = new EdgeQL(`select sys_form::Form {
+			id, name, header, subHeader, fields: {*},
+			actions: {type := .codeType.name, query, filterItems: { dbName, source := .codeSource.name, sourceKey, data_type := .codeDataType.name, op := .codeOp.name
+		}}}`)
+		query.addFilter('id', EdgeDBDataType.uuid, formId)
+		return await dbSelectSingle(query)
+	}
+
+	async function getFormData(formActionType: string, formActions: FormAction[], data = {}) {
+		const action = formActions.find((fa: FormAction) => fa.type === formActionType)
+		if (action) {
+			switch (formActionType) {
+				case 'insert':
+					break
+
+				case 'select':
+					return await getFormDataSelect(action, data)
+					break
+			}
+		}
+	}
+
+	async function getFormDataSelect(formAction: FormAction, data: {}) {
+		const query = new EdgeQL(formAction.query)
+		return await dbSelect(query)
+	}
+}
+
 export async function getNodesByParent(parentNodeId: string) {
 	const query = new EdgeQL(`select sys_app::Node {
-    type := .code_type.name, order, id, name, label, icon := .code_icon.name,  obj_id := .obj.id, obj_link := .obj[is sys_app::Page].link
+    id, type := .codeType.name, name, header, icon := .codeIcon.name,  page, component := .codeComponent.name, obj_id := .obj.id, order
   }`)
 	query.addFilter('parent.id', EdgeDBDataType.uuid, parentNodeId)
 	query.addOrder('order', EdgeDBFilterDirection.asc)
@@ -18,11 +47,24 @@ export async function getNodesByParent(parentNodeId: string) {
 }
 
 export async function getUserEdge(userName: string) {
+	// get user
 	const query = new EdgeQL(`select sys_user::User {
-		id, last_name, first_name, full_name, username,
-		resource_home_screen_widgets := (select .user_types.resources[is sys_app::HomeScreen].widgets {id, name}),
-		resource_programs := (select .user_types.resources[is sys_app::Node] {type := 'program', order, id, name, label, icon := .code_icon.name, } filter not exists .parent order by .order)
-	}`)
+		id, lastName, firstName, fullName, username,
+		resource_home_screen_widgets := (select .userTypes.resources[is sys_app::HomeScreen].widgets {id, name}),
+		resource_programs := (select .userTypes.resources[is sys_app::Node] { id, type := .codeType.name, name, header, icon := .codeIcon.name, page, component := .codeComponent.name, obj_id := .obj.id, order} filter not exists .parent order by .order)
+}`)
 	query.addFilter('username', EdgeDBDataType.str, userName)
-	return await dbSelectSingle(query)
+	const user = await dbSelectSingle(query)
+	// console.log('getUserEdge...')
+	// console.log('userId1:', user)
+
+	// set global current user
+	// await dbExecute(`set global sys_user::currentUserId := <uuid>"${user.id}"`)
+	// set global currentUserId := <uuid>"9a2966ba-4e96-11ee-abc0-73f75479eb42";
+
+	// const q = new EdgeQL(`select global sys_user::currentUser { fullName }`)
+	// const u = await dbSelectSingle(q)
+	// console.log('global user:', u)
+	// await getData('')
+	return user
 }
