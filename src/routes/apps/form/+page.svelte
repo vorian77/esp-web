a401830eab40d6b91540d57456882df0c57c5c45
Phyllip Hall <phall776@gmail.com>
2023-09-20 10:53:19 -0400

    feat: migrate form page to /apps component

--- src/routes/apps/form/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { navNodeCurrent } from '$comps/nav/navStore'
-	import NavBack from '$comps/nav/NavBack.svelte'
-</script>
-
-<h1>Form</h1>
-SelectedNode:
-<pre>{JSON.stringify($navNodeCurrent, null, 2)}</pre>
