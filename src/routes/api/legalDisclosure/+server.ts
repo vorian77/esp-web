046e906013bbd9bbf40c69c6e77a5c7342801ba0
Phyllip Hall <phall776@gmail.com>
2023-09-20 08:53:55 -0400

    maint: cleaned up legalDisclosure

--- src/routes/api/legalDisclosure/+server.ts
+++ src/routes/api/legalDisclosure/+server.ts
@@ -1,12 +1,7 @@
 import { HTMLMETHOD } from '$comps/types'
 import { dbESPAPI } from '$server/dbESP'
-import { asUpsert, asGet } from '$lib/utils/utils'
 
 export async function POST({ request }) {
-	// reset user object in locals to force re-retrieval
-	asUpsert('user', undefined)
-
-	// update user's disclosure flag
 	const method = HTMLMETHOD.POST
 	const procedure = 'ws_cm_ssr_disclosure'
 
