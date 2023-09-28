fa18015017ce6e52f7cbbb9590593be89ce92b8d
Phyllip Hall <phall776@gmail.com>
2023-09-28 06:40:12 -0400

    maint: remove unused "fieldElements" from Form

--- src/lib/components/esp/form/FormDetail.svelte
+++ src/lib/components/esp/form/FormDetail.svelte
@@ -20,14 +20,13 @@
 	import { error } from '@sveltejs/kit'
 
 	const dispatch = createEventDispatcher()
+
 	const FILENAME = '$comps/esp/form/FormDetail.svelte'
 	const FORM_NAME = ''
 	const SUBMIT_BUTTON_NAME = 'SUBMIT_BUTTON_NAME'
 
 	export let formObj: Form
 	export let surface = ''
-
-	let fieldElements = []
 
 	const classPopup = formObj.popup ? 'grid grid-cols-10 gap-4' : ''
 	const classPopupHeader = formObj.popup ? 'col-span-9' : ''
@@ -89,39 +88,39 @@
 		dispatch('formSubmitted', { formName: formObj.name, ...response.data })
 	}
 
-	export function getField(fieldName: string) {
-		const field = formObj.fields.find((f) => f.name == fieldName)
-		if (!field) {
-			throw error(500, {
-				file: FILENAME,
-				function: 'getField',
-				message: `Unable to find field: ${fieldName}.`
-			})
-		}
-		return fieldElements[field.index]
-	}
 	function cancelForm(event: MouseEvent) {
 		dispatch('formCancelled')
 	}
 </script>
 
 <div class={surface}>
-	{#if formObj.header}
-		<div class={classPopup}>
-			<div class={classPopupHeader}>
-				<h1 class="h1 {formObj.subHeader ? '' : 'mb-5'}">{formObj.header}</h1>
-			</div>
-			{#if formObj.popup}
-				<div class="justify-self-end">
-					<button
-						type="button"
-						class="btn-icon btn-icon-sm variant-filled-error"
-						on:click={cancelForm}>X</button
-					>
+	<div class="flex justify-between">
+		<div>
+			{#if formObj.header}
+				<div class={classPopup}>
+					<div class={classPopupHeader}>
+						<h1 class="h1 {formObj.subHeader ? '' : 'mb-5'}">{formObj.header}</h1>
+					</div>
 				</div>
 			{/if}
 		</div>
-	{/if}
+
+		<div class="flex">
+			<div>
+				<slot name="actions" />
+			</div>
+			<div>
+				{#if formObj.popup}
+					<button
+						type="button"
+						class="btn-icon btn-icon-sm variant-filled-error ml-2"
+						on:click={cancelForm}>X</button
+					>
+				{/if}
+			</div>
+		</div>
+	</div>
+
 	{#if formObj.subHeader}
 		<div class="mb-5">
 			<p class="text-sm text-gray-500">
@@ -135,54 +134,32 @@
 			<div class:mt-3={idx}>
 				{#if field.element === FieldElement.header}
 					<div class:mt-9={idx}>
-						<FormElHeader bind:field bind:this={fieldElements[idx]} formValues={formObj.values} />
+						<FormElHeader bind:field formValues={formObj.values} />
 					</div>
 				{:else if field.element === FieldElement.input}
 					{#if field.type === 'checkbox'}
-						<FormElInpCheckbox
-							bind:field
-							bind:this={fieldElements[idx]}
-							on:click={validateFieldCheckbox}
-						/>
+						<FormElInpCheckbox bind:field on:click={validateFieldCheckbox} />
 					{:else if field.type === 'radio'}
-						<FormElInpRadio
-							bind:field
-							bind:this={fieldElements[idx]}
-							on:change={validateFieldBase}
-						/>
+						<FormElInpRadio bind:field on:change={validateFieldBase} />
 					{:else}
-						<FormElInp
-							bind:field
-							bind:this={fieldElements[idx]}
-							on:change={validateFieldBase}
-							on:keyup={keyUp}
-						/>
+						<FormElInp bind:field on:change={validateFieldBase} on:keyup={keyUp} />
 					{/if}
 				{:else if field.element === FieldElement.pictureTake}
-					<FormElPictureTake
-						bind:field
-						bind:this={fieldElements[idx]}
-						on:change={validateFieldBase}
-					/>
+					<FormElPictureTake bind:field on:change={validateFieldBase} />
 				{:else if field.element === FieldElement.select}
-					<FormElSelect bind:field bind:this={fieldElements[idx]} on:change={validateFieldBase} />
+					<FormElSelect bind:field on:change={validateFieldBase} />
 				{:else if field.element === FieldElement.textArea}
-					<FormElTextarea
-						bind:field
-						bind:this={fieldElements[idx]}
-						on:change={validateFieldBase}
-						on:keyup={keyUp}
-					/>
+					<FormElTextarea bind:field on:change={validateFieldBase} on:keyup={keyUp} />
 				{/if}
 			</div>
 
 			{#if formObj.fields[idx].validity.level == ValidityErrorLevel.error}
 				<div class="text-error-500 mb-3">
-					<p class="">{formObj.fields[idx].validity.message}</p>
+					<p>{formObj.fields[idx].validity.message}</p>
 				</div>
 			{:else if formObj.fields[idx].validity.level == ValidityErrorLevel.warning}
 				<div class="text-warning-500 mb-3">
-					<p class="">{formObj.fields[idx].validity.message}</p>
+					<p>{formObj.fields[idx].validity.message}</p>
 				</div>
 			{/if}
 		{/each}
