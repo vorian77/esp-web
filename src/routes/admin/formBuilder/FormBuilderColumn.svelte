8b4383f263d74b1f331f4de16637bd1cce210db5
Phyllip Hall <phall776@gmail.com>
2024-01-08 17:58:12 -0500

    deploy bug: commented old code

--- src/routes/admin/formBuilder/FormBuilderColumn.svelte
+++ src/routes/admin/formBuilder/FormBuilderColumn.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import { Form as FormClass } from '$comps/dataObj/dataObjOld'
+	// import { Form as FormClass } from '$comps/dataObj/dataObjOld'
 	import Form from '$comps/form/FormDetail.svelte'
 
 	export let data
 
-	let formCol = new FormClass(data.defnFormCol)
+	// let formCol = new FormClass(data.defnFormCol)
 </script>
 
-<Form bind:formObj={formCol} />
+<!-- <Form bind:formObj={formCol} /> -->
