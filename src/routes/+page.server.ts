1ed6da926661462e611c74938999e3dcfc0af06e
Phyllip Hall <phall776@gmail.com>
2024-02-03 07:12:11 -0500

    maint: remove full db init

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,5 @@
 import { getEnvVar } from '$server/env'
-import { init } from '$server/dbEdge/init/dbEdgeInit'
 
 export async function load() {
-	// <temp>  230908 - database init
-	await init()
-
 	return { system: { org_name: getEnvVar('ORG_NAME'), server_mode: getEnvVar('SERVER_MODE') } }
 }
