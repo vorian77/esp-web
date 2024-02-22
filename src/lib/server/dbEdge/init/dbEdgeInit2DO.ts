81af4ae2b14b3c6daa19ff343d8005187985b481
Phyllip Hall <phall776@gmail.com>
2024-02-22 08:37:21 -0500

    fix: import error on dbEdgeInit2DO

--- src/lib/server/dbEdge/init/dbEdgeInit2DO.ts
+++ src/lib/server/dbEdge/init/dbEdgeInit2DO.ts
@@ -1,6 +1,6 @@
 import { sectionHeader } from '$server/dbEdge/init/dbEdgeInitUtilities1'
-import initDOCMStudent from '$server/dbEdge/init/dbEdgeInit2DOCMStudent'
-import initDOCMTraining from '$server/dbEdge/init/dbEdgeInit2DOCMTraining'
+import { initDOCMStudent } from '$server/dbEdge/init/dbEdgeInit2DOCMStudent'
+import { initTraining } from '$server/dbEdge/init/dbEdgeInit2DOCMTraining'
 import initDOReport from '$server/dbEdge/init/dbEdgeInit2DOReport'
 import initDOSysAdmin from '$server/dbEdge/init/dbEdgeInit2DOSysAdmin'
 import initDOSysAuth from '$server/dbEdge/init/dbEdgeInit2DOSysAuth'
@@ -8,7 +8,7 @@ import initDOSysAuth from '$server/dbEdge/init/dbEdgeInit2DOSysAuth'
 export async function initDO() {
 	sectionHeader('DataObj')
 	await initDOCMStudent()
-	await initDOCMTraining()
+	await initTraining()
 	await initDOReport()
 	await initDOSysAdmin()
 	await initDOSysAuth()
